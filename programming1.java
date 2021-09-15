1c2812938cbe515634e1470ab718c0afd449c46e
Josh Chen <90787021+FlashyyCS@users.noreply.github.com>
2021-09-15 11:19:35 -0500

    Add files via upload

---
+++ programming1.java
@@ -0,0 +1,27 @@
+/* this is
+a multi line
+comment */
+
+//and this is a single line comment
+
+// Josh Chen
+// 9-15-2021
+
+
+public class programming1 {
+    public static void main(String[] args) {
+      System.out.println("");
+      System.out.println("Ms. Hunt said \"This is awesome\" \nbut I disagree");
+      System.out.println("");
+      System.out.println("Special characters can be accessed by \\u and then the unicode");
+      System.out.println("Here is an example: \\u00b0 will return \u00b0");
+      System.out.println("  __      _" );
+      System.out.println("o'')}____//");
+      System.out.println(" `_/      )");
+      System.out.println(" (_(_/-(_/");
+      System.out.println("");
+      System.out.print("That was a dog");
+      System.out.println(" by the way");
+      System.out.println("");
+    }
+  }
---
+++ reference.java
@@ -0,0 +1,21 @@
+/* this is a multi
+line comment */
+
+// Josh Chen
+// 9-15-2021
+
+
+public class reference {
+  public static void main(String[] args) {
+    System.out.println("");
+    System.out.println("Ms. Hunt said \"This is awesome\" \nbut I disagree");
+    System.out.println("");
+    System.out.println(" /\\__/\\" );
+    System.out.println(" ( ^.^) ");
+    System.out.println(" |=  =| ");
+    System.out.println(" ------");
+    System.out.println("");
+    System.out.println("That was a cat by the way");
+    System.out.println("");
+  }
+}
